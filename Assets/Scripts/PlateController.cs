d9f3911979d36acd7dd995ebb413c2d84c26e500
Marc Freitag <m.freitag@gmx.net>
2022-11-06 02:08:59 +0100

    Menu wird auf Teller gehalten

--- Assets/Scripts/PlateController.cs
+++ Assets/Scripts/PlateController.cs
@@ -8,13 +8,18 @@ public class PlateController : MonoBehaviour
      * Sitzt auf der Plate
      * Speichert das Menu, welches auf ihm liegt
      */
+    private StackingColliderController _stackingColliderController;
     void Start()
     {
-        
+        _stackingColliderController = GetComponent<StackingColliderController>();
     }
     
     void Update()
     {
+        for (int i = 0; i < _stackingColliderController.ingredientNames.Length; i++)
+        {
+            print("Names: " + _stackingColliderController.ingredientNames[i]);
+        }
         
     }
 }
---
+++ Assets/StackingColliderController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StackingColliderController : MonoBehaviour
+{
+    [SerializeField] private Collider[] menuStackItems;
+    public string[] ingredientNames = new string[4];
+    private int counter = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        menuStackItems[counter].gameObject.SetActive(true);
+        if(counter < menuStackItems.Length - 1) counter++;
+    }
+
+    public Collider[] GetStackItems()
+    {
+        return menuStackItems;
+    }
+}
