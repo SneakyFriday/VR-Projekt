bfdf417be4813a9ffa19ae5db0d676e822c20a36
Marc Freitag <m.freitag@gmx.net>
2022-11-22 00:43:27 +0100

    fix: Menu Items fixieren
    Items auf der Platte werden nun richtig fixiert

--- Assets/Scripts/VR-Player/FixMenuElementsOnPlate.cs
+++ Assets/Scripts/VR-Player/FixMenuElementsOnPlate.cs
@@ -1,22 +1,20 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class FixMenuElementsOnPlate : MonoBehaviour
 {
     private Queue<GameObject> _menuItems = new();
     private List<GameObject> _items = new();
+    private List<String> _itemNames = new();
     [SerializeField] private GameObject menuItemParent;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider coll)
     {
-        // if (other.CompareTag("Ingredient"))
+        // if (coll.CompareTag("Ingredient"))
         // {
-        //    _menuItems.Enqueue(other.gameObject);
-        //     print("New GameObject in Queue: " + other.gameObject.name);
+        //    _menuItems.Enqueue(coll.gameObject);
+        //     print("New GameObject in Queue: " + coll.gameObject.name);
         //     foreach (var item in _menuItems)
         //     {
         //        print("Current Queue Items: " + item.name);
@@ -24,27 +22,47 @@ public class FixMenuElementsOnPlate : MonoBehaviour
         //     print("Size:" + _menuItems.Count);
         //     
         // }
-    }
-
-    private void OnCollisionEnter(Collision coll)
-    {
-        if (coll.gameObject.CompareTag("Ingredient") || coll.gameObject.CompareTag("IngredientCooked"))
+        
+        if (coll.CompareTag("Ingredient") || coll.CompareTag("IngredientCooked"))
         {
             _items.Add(coll.gameObject);
-            print("Item added to list: " + coll.gameObject.name);
+            _itemNames.Add(coll.name);
+            print("Item added to list: " + coll.name);
             print("Size: " + _items.Count);
         }
     }
 
-    private void OnCollisionExit(Collision coll)
+    private void OnTriggerExit(Collider coll)
     {
-        if (coll.gameObject.CompareTag("Ingredient") || coll.gameObject.CompareTag("IngredientCooked"))
+        if (coll.CompareTag("Ingredient") || coll.CompareTag("IngredientCooked"))
         {
-            _items.Remove(coll.gameObject);
-            print("Item removed from list: " + coll.gameObject.name);
-            print("Size: " + _items.Count);
+          _items.Remove(coll.gameObject);
+          _itemNames.Remove(coll.name);
+          print("Item removed from list: " + coll.name);
         }
+        
     }
+
+    // private void OnCollisionEnter(Collision coll)
+    // {
+    //     if (coll.gameObject.CompareTag("Ingredient") || coll.gameObject.CompareTag("IngredientCooked"))
+    //     {
+    //         _items.Add(coll.gameObject);
+    //         print("Item added to list: " + coll.gameObject.name);
+    //         print("Size: " + _items.Count);
+    //     }
+    // }
+    
+
+    // private void OnCollisionExit(Collision coll)
+    // {
+    //     if (coll.gameObject.CompareTag("Ingredient") || coll.gameObject.CompareTag("IngredientCooked"))
+    //     {
+    //         _items.Remove(coll.gameObject);
+    //         print("Item removed from list: " + coll.gameObject.name);
+    //         print("Size: " + _items.Count);
+    //     }
+    // }
 
     public void FixateMenuOnPlate()
     {
@@ -54,20 +72,22 @@ public class FixMenuElementsOnPlate : MonoBehaviour
             item.GetComponent<Rigidbody>().isKinematic = true;
             item.GetComponent<Collider>().enabled = false;
             item.transform.SetParent(transform, true);
-            print("Fixing Items on: " + transform.name + " for Ingredient: " + item.name);
         }
     }
 
     public void ReleaseMenuFromPlate()
     {
-        
         print("Release Plate");
+        transform.DetachChildren();
         foreach (var item in _items)
         {
             item.GetComponent<Rigidbody>().isKinematic = false;
             item.GetComponent<Collider>().enabled = true;
-            item.transform.SetParent(menuItemParent.transform, true);
-            print("Releasing Items to: " + menuItemParent.name + " for Ingredient: " + item.name);
         }
+    }
+
+    public List<GameObject> CheckMenu()
+    {
+        return _items;
     }
 }
