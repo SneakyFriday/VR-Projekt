29c1338502aed10fd2e61ce92393e4229fcb9431
Marc Freitag <m.freitag@gmx.net>
2022-12-27 02:12:01 +0100

    fix: Score updated + Pathfinding

---
+++ Assets/Scripts/GUI/PointsController.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class PointsController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI currentScoreTxt;
+    //private int _currentScore;
+    //private int _burgerScore = 100;
+
+    private void Start()
+    {
+        //_currentScore = 0;
+        currentScoreTxt.text = "Score: ";
+    }
+
+    // public void SetScore()
+    // {
+    //     print("Player Scores!");
+    //     _currentScore += _burgerScore;
+    //     print(_currentScore);
+    //     currentScoreTxt.text = "Score: " + _currentScore;
+    // }
+}
